9693f7c09609d4a1cc0c93751059ce012254bb3b
James <znzldh1234@gmail.com>
2022-03-09 22:10:47 +0900

    Add project files.

---
+++ boj/1000_A+B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int problem_1000()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	int m;
+	cin >> n >> m;
+	cout << n + m << '\n';
+
+	return 0;
+}
---
+++ boj/2558_A+B-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int A;
+	int B;
+
+	cin >> A;
+	cin >> B;
+	cout << A + B << '\n';
+
+	return 0;
+}
