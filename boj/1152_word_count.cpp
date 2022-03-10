f0d4b9d26223d6349b498432091f53667f077636
James <znzldh1234@gmail.com>
2022-03-10 14:30:17 +0900

    1152 solution

--- boj/1152_word_count.cpp
+++ boj/1152_word_count.cpp
@@ -1,21 +1,47 @@
 #include <iostream>
+#include <string>
 
-using std::string;
+#define TRIM_SIGN	" \t\n\r\v\f"
 
-#define TRIM_SPACE	" \t\n\r\v\f"
-
-inline string& ltrim(string& str, const char* drop = TRIM_SPACE)
+inline std::string& lTrim(std::string& str, const char* drop = TRIM_SIGN)
 {
-	str.erase(0, str.find_first_not_of(drop + 1));
+	str.erase(0, str.find_first_not_of(drop));
+	return str;
 }
 
-int main(void)
+inline std::string& rTrim(std::string& str, const char* drop = TRIM_SIGN)
+{
+	str.erase(str.find_last_not_of(drop) + 1);
+	return str;
+}
+
+inline std::string& Trim(std::string& str, const char* drop = TRIM_SIGN)
+{
+	rTrim(lTrim(str));
+	return str;
+}
+
+int problem_1152()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	string strWord;
+	std::string str = "";
+	int nCount = 0;
 
-
+	std::getline(std::cin, str);
+	Trim(str, " ");
+	if (str.length() > 0)
+	{
+		++nCount;
+	}
+	for (int i = 0; i < str.length(); ++i)
+	{
+		if (str[i] == ' ')
+		{
+			++nCount;
+		}
+	}
+	std::cout << nCount << '\n';
 	return 0;
 }
