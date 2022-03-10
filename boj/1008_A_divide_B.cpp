9c7b37de36c520e3421bfefcd52c89e2f887a9e2
James <znzldh1234@gmail.com>
2022-03-10 10:37:47 +0900

    1008 solution

---
+++ boj/1008_A_divide_B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int problem_1008()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << setprecision(10);
+
+	double A;
+	double B;
+	cin >> A >> B;
+	cout << A / B << '\n';
+
+	return 0;
+}
