d233c0c76336fb14dcbd15de28a0ad65eeee2daf
James <znzldh1234@gmail.com>
2022-03-10 10:27:02 +0900

    1001 solution

---
+++ boj/1001_A-B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int problem_1001()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int A;
+	int B;
+	cin >> A >> B;
+	cout << A - B << '\n';
+
+	return 0;
+}
