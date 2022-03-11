506f02b3a440b1cf744d460f8b5119a46598b0da
James <znzldh1234@gmail.com>
2022-03-12 08:52:30 +0900

    10171 solution

---
+++ boj/10171_cat.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int problem_10171()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "\\    /\\\n" << " )  ( ')\n" << "(  /  )\n" << " \\(__)|\n";
+
+	return 0;
+}
