d7c9112d2568225744f512fff5c910bf73a5bbac
James <znzldh1234@gmail.com>
2022-03-11 21:04:19 +0900

    2742 solution

---
+++ boj/2742_reverse_put_N.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int problem_2742()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N = 0;
+	cin >> N;
+	for (int i = N; i > 0; --i)
+	{
+		cout << i << "\n";
+	}
+
+	return 0;
+}
