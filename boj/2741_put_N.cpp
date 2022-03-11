1a9c38670995b3f64074127469fb9605d7c15da4
James <znzldh1234@gmail.com>
2022-03-11 21:00:31 +0900

    2741 solution

---
+++ boj/2741_put_N.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int problem_2741()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N = 0;
+	cin >> N;
+
+	for (int i = 1; i <= N; ++i)
+	{
+		cout << i << "\n";
+	}
+
+	return 0;
+}
