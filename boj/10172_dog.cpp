b75a4337252591404f0c9c3920366f967cd2b086
James <znzldh1234@gmail.com>
2022-03-12 08:57:59 +0900

    10172 solution

---
+++ boj/10172_dog.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int problem_10172()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "|\\_/|\n" << "|q p|   /}\n" << "( 0 )\"\"\"\\\n"
+		<< "|\"^\"`    |\n" << "||_/=\\\\__|\n";
+
+	return 0;
+}
