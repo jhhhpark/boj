d394b557ec1a11a07e2221d4416b1555b9963360
James <znzldh1234@gmail.com>
2022-03-12 09:28:03 +0900

    10809

---
+++ boj/10809_find_alpha.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int aAlpha[26] = { 0, };
+	char aWord[101] = { 0, };
+	cin >> aWord;
+
+	int pos = 0;
+	while (aWord[pos] != '\0')
+	{
+		int ndx = aWord[pos] - 'a';
+		if (aAlpha[ndx] == 0)
+		{
+			aAlpha[ndx] = pos + 1;
+		}
+		++pos;
+	}
+
+	for (int i = 0; i < sizeof(aAlpha) / sizeof(int); ++i)
+	{
+		cout << aAlpha[i] - 1 << " ";
+	}
+
+	return 0;
+}
