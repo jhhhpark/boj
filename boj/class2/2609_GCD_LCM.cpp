d141d32002d5690997d19ab8f0a46c17f66c7e8a
James <znzldh1234@gmail.com>
2022-04-27 13:24:10 +0900

    2609 solution

---
+++ boj/class2/2609_GCD_LCM.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n1 = 0;
+	int n2 = 0;
+	int GCD = 0;
+	int LCM = 0;
+	cin >> n1 >> n2;
+
+	int t1 = n1;
+	int t2 = n2;
+	while (t1 % t2 != 0)
+	{
+		int mod = t1 % t2;
+		t1 = t2;
+		t2 = mod;
+	}
+	GCD = t2;
+	LCM = n1 * n2 / t2;
+
+	cout << GCD << '\n' << LCM << '\n';
+
+	return 0;
+}
