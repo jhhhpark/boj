9e914eed1d594f80bce5638e8b4eeec4daa7081a
James <znzldh1234@gmail.com>
2022-04-30 14:34:11 +0900

    2869 solution

---
+++ boj/class2/2869_climb_snail.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int A = 0;
+	int B = 0;
+	int V = 0;
+	int result = 0;
+	cin >> A >> B >> V;
+
+	cout << (V - B - 1) / (A - B) + 1<< endl;
+
+	return 0;
+}
