755dc766a9126d2a4e9672b3cfb74ce315d0601b
James <znzldh1234@gmail.com>
2022-06-03 09:05:43 +0900

    1463 solution

--- boj/class3/1463_make_one.cpp
+++ boj/class3/1463_make_one.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
+#define MIN(a, b) (a < b ? a : b)
+
+int F(int N)
+{
+	if (N <= 1) return 0;
+	int s1 = F(N / 3) + N % 3 + 1;
+	int s2 = F(N / 2) + N % 2 + 1;
+	return MIN(s1, s2);
+}
+
+int problem_1463()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+	int N = 0, min = 0;
+	cin >> N;
 
-
+	min = F(N);
+	cout << min << '\n';
 	return 0;
 }
+
