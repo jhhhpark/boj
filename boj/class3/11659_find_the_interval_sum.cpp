9522900bd744b06d5968ba53ac74f1e82f545531
James <znzldh1234@gmail.com>
2022-09-19 11:25:27 +0900

    11659 solution

---
+++ boj/class3/11659_find_the_interval_sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int arr[100001] = { 0, };
+	int N, M, i, j;
+	int idx = 0;
+	cin >> N >> M;
+	for (idx = 1; idx <= N; ++idx)
+	{
+		cin >> arr[idx];
+		arr[idx] += arr[idx - 1];
+	}
+
+	idx = 0;
+	while (idx < M)
+	{
+		cin >> i >> j;
+		cout << arr[j] - arr[i - 1] << '\n';
+		++idx;
+	}
+
+	return 0;
+}
