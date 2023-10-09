93109aec152b34f50567f9ec623c40a992611cac
__jihoon_park <znzldh1234@gmail.com>
2023-10-10 08:47:21 +0900

    25083 solution

---
+++ boj/class1/25083_print_sprout.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+	return 0;
+}
